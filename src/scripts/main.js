be108bbcd51b5a01ce918289680dcc434a62176f
Tata2222 <tataolcha@gmail.com>
2020-03-19 01:22:05 +0200

    3section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const navBlock = document.querySelector('#header-nav');
+const navList = document.querySelector('#nav-list');
+const navLinks = document.querySelectorAll('.nav__link');
+const btnMenu = document.querySelector('#nav-btn');
+
+btnMenu.addEventListener('click', () => {
+  navBlock.classList.toggle('header__nav--show');
+  navList.classList.toggle('nav__list--mobile');
+  btnMenu.classList.toggle('header__nav-btn--close');
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navBlock.classList.remove('header__nav--show');
+    btnMenu.classList.remove('header__nav-btn--close');
+  });
+});
